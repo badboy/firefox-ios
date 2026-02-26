ef85e1aa4d910900fd1eb0888cefcaf58b2d96ca
Jan-Erik Rediger <jrediger@mozilla.com>
2026-02-26 11:54:27 +0100

    test values directly, not via ping submit

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Nimbus/RecordedNimbusContextTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Nimbus/RecordedNimbusContextTests.swift
@@ -100,18 +100,12 @@ class RecordedNimbusContextTests: XCTestCase {
             cannotUseAppleIntelligence: true
         )
 
-        var value: GleanMetrics.NimbusSystem.RecordedNimbusContextObject?
-        let expectation = expectation(description: "The Firefox Suggest ping was sent")
-        NimbusGleanPings.nimbusTargetingContext.testBeforeNextSubmit { e in
-            value = GleanMetrics.NimbusSystem.recordedNimbusContext.testGetValue()
-            expectation.fulfill()
-        }
+        _ = NimbusGleanPings.nimbusTargetingContext
 
         recordedContext.setEventQueryValues(eventQueryValues: [RecordedNimbusContext.DAYS_OPENED_IN_LAST_28: 1.5])
         recordedContext.record()
-        NimbusGleanPings.nimbusTargetingContext.submit()
 
-        wait(for: [expectation], timeout: 5.0)
+        let value = GleanMetrics.NimbusSystem.recordedNimbusContext.testGetValue()
 
         XCTAssertNotNil(value)
         XCTAssertEqual(value?.appVersion, recordedContext.appVersion)
